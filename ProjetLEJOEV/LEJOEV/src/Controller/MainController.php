defc54efa3c9bd72b76ae88fc624189ff92f1901
Rigaudeau Léo <leo.rigaudeau@ynov.com>
2018-12-12 10:48:09 +0100

    recup insert

--- ProjetLEJOEV/LEJOEV/src/Controller/MainController.php
+++ ProjetLEJOEV/LEJOEV/src/Controller/MainController.php
@@ -2,13 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\UtilisateursRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
+    public  $viewData =[];
+    private  $usersRepo;
+
+    public function __construct(UtilisateursRepository $usersRepo)
+    {
+        $this->usersRepo =$usersRepo;
+
+    }
     /**
-     * @Route("/main", name="main")
+     * @Route("", name="main")
      */
     public function index()
     {
@@ -16,4 +25,16 @@ class MainController extends AbstractController
             'controller_name' => 'MainController',
         ]);
     }
+
+    /**
+     * @Route("/{name}", name="mainname")
+     */
+    public function indexname($name)
+    {
+        $viewData['utilisateur']=$this->usersRepo->find($name);
+
+        return $this->render('main/index.html.twig',$viewData);
+    }
+
+
 }
--- ProjetLEJOEV/LEJOEV/src/Repository/UtilisateursRepository.php
+++ ProjetLEJOEV/LEJOEV/src/Repository/UtilisateursRepository.php
@@ -19,32 +19,4 @@ class UtilisateursRepository extends ServiceEntityRepository
         parent::__construct($registry, Utilisateurs::class);
     }
 
-    // /**
-    //  * @return Utilisateurs[] Returns an array of Utilisateurs objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('u')
-            ->andWhere('u.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('u.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Utilisateurs
-    {
-        return $this->createQueryBuilder('u')
-            ->andWhere('u.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
